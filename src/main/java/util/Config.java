099f74eb1dc28d083f07c087c9ee51ca45068372
Vadim Ryabov <mr.holeyko@gmail.com>
2022-10-11 20:20:33 +0300

    updated Config

--- src/main/java/util/Config.java
+++ src/main/java/util/Config.java
@@ -1,6 +1,7 @@
-package help;
+package util;
 
 public class Config {
+    public static final String SHEET_FILE_FORMAT = "xlsx";
     public static final String PATH_TO_RESOURCES = "../data/";
     public static final String BOT_NAME = "Lexillize Add Words Bot";
 }
