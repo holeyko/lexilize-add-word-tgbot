80e759ea7c88230c9718b0ad5e81ab2af278c487
Vadim Ryabov <mr.holeyko@gmail.com>
2022-10-11 20:23:01 +0300

    added Main and remove FileController from help folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
 import bot.Bot;
-import help.Config;
-import help.FilesController;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import util.Config;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,8 +10,7 @@ public class Main {
             TelegramBotsApi TGBotsAPI = new TelegramBotsApi(DefaultBotSession.class);
             TGBotsAPI.registerBot(new Bot(
                     Config.BOT_NAME,
-                    System.getenv("BOT_TOKEN"),
-                    new FilesController(Config.PATH_TO_RESOURCES)
+                    System.getenv("BOT_TOKEN")
             ));
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
